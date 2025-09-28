77ffb8920850e1f6114cb9aac6343d102e7b2d80
Sherry Du <sherryd0936@gmail.com>
2025-09-27 20:09:10 -0400

    Fixed rawdata script

--- 01_scripts/r_scripts/Du_FiddlerCrabProject_Script-acquire-raw-data_V0.R
+++ 01_scripts/r_scripts/Du_FiddlerCrabProject_Script-acquire-raw-data_V0.R
@@ -1,4 +1,4 @@
-# Getting raw data
+# Getting raw data from package
 
 # Required R packages
 library(lterdatasampler)
@@ -8,3 +8,12 @@ getwd()
 
 # Save dataset as .csv into 00_rawdata folder
 write.csv(pie_crab,"./00_rawdata/Du_FiddlerCrabProject_Data-raw_V0.csv", row.names = FALSE)
+
+# Import data
+CrabData <- 
+  read.table('./00_rawdata/Du_FiddlerCrabProject_Data-raw_V0.csv', 
+             header=TRUE, stringsAsFactors=TRUE, sep=",", na.strings=c(""," ","NA"), dec=".", 
+             strip.white=TRUE)
+
+# View data
+View(CrabData)
